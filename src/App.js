4f45569b0699eefa8d1e38cdcb42937f620664ee
hossainalamin <hossainalamin980@gmail.com>
2022-12-05 23:44:45 +0600

    Components with probs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-const personOne = {
-  name : "Al Amin",
-  job : "Programmer"
-}
-
-const personTwo = {
-  name : "Yeasin",
-  job : "Manager"
-}
 const style = {
   backgroundColor : "cyan",
   border : '2px'  
 }
 function App() {
+  const personOne = {
+    name : "Al Amin",
+    job : "Programmer"
+  }
+  
+  const personTwo = {
+    name : "Yeasin",
+    job : "Manager"
+  }
+  
+  const personThree = {
+    name : "kanon",
+    job : "Rider"
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2 style={style}>Name : {personOne.name} Job : {personOne.job}</h2>
-        <p style = {{backgroundColor : 'red'}}>Name : {personTwo.name} Job : {personTwo.job}</p>
+        <Person name={personOne.name} role={personOne.job}></Person>
+        <Person name={personTwo.name} role={personTwo.job}></Person>
+        <Person name={personThree.name} role={personThree.job}></Person>
       </header>
     </div>
   );
+}
+const styleOne = {
+  border : "2px solid red",
+  padding : "2px"
+}
+function Person(props){
+  return (
+    <div style={styleOne}>
+      <h1>Name : {props.name}</h1>
+      <p>Role : {props.role}</p>
+    </div>
+  )
 }
 
 export default App;
