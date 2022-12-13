eba859f1acaf5a3ba0b2b022366fcd3ba25cec72
hossainalamin <hossainalamin980@gmail.com>
2022-12-13 23:56:03 +0600

    more state

--- src/App.js
+++ src/App.js
@@ -70,4 +70,24 @@ function UserData()
     </div>
   )
 }
+
+function UserData()
+{
+  const[user, setUser] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUser(data))
+  },[])
+  return(
+    <div>
+    {
+      user.map(user => user.name)
+    }
+    {
+      user.map(user => <li>Email : {user.email}</li>)
+    }
+    </div>
+  )
+}
 export default App;
